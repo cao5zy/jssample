42e73366659c991766e92b419275a11aedff911f
Zongying Cao <zongying.cao@hpe.com>
2017-12-29 10:57:25 +0800

    add seneca express example

--- src/seneca/seneca_express.js
+++ src/seneca/seneca_express.js
@@ -1,12 +1,24 @@
-const Seneca = require('seneca')
-const SenecaWeb = require('seneca-web')
-const Express = require('express')
-const seneca = Seneca()
-seneca.use(SenecaWeb, {
-  context: Express(),
-  adapter: require('seneca-web-adapter-express')
-})
-seneca.ready(() => {
-  const app = seneca.export('web/context')()
-  app.listen(3000)
-})
+'use strict'
+
+var Seneca = require('seneca')
+var Express = require('express')
+var Web = require('seneca-web')
+var Routes = require('./seneca_express_routes')
+var Plugin = require('./seneca_express_plugin')
+
+var config = {
+  routes: Routes,
+  adapter: require('seneca-web-adapter-express'),
+  context: Express()
+}
+
+var seneca = Seneca()
+  .use(Plugin)
+  .use(Web, config)
+  .ready(() => {
+    var server = seneca.export('web/context')()
+
+    server.listen('4000', () => {
+      console.log('server started on: 4000')
+    })
+  })
